325b094dadd065b18198021e038ae933f00a96a3
shini55 <tharshini2312@hotmail.com>
2025-04-01 19:58:16 +0800

    Update WeatherLogType.h

--- WeatherLogType.h
+++ WeatherLogType.h
@@ -41,30 +41,8 @@ public:
     */
     int LoadRecords(const std::string &filename);
 
-
-
-    /**
-    * @brief Prints the calculated average and standard deviation to the screen.
-    * @param choice The type of weather parameter to print.
-    * @param month The month for which data is printed.
-    * @param year The year for which data is printed.
-    */
-    void printAverageAndStdDevToScreen(int choice, int month,int year) const;
-
-    /**
-    * @brief Prints the calculated average and total solar radiation to a file.
-    * @param choice The type of weather parameter to print.
-    * @param month The month for which data is printed.
-    * @param year The year for which data is printed.
-    */
-    void printAverageAndTotalToFile(int choice, int month,int year) const;
-
-    /**
-    * @brief Prints all loaded weather data to a specified output file.
-    * @param outputFilename The name of the output file.
-    */
-    void printLoadedDataToFile(const std::string &outputFilename) const;
-
+    // Add this public getter method
+    const Map<int, Map<int, Vector<WeatherRecType>>>& GetWeatherData() const;
 
 
 
@@ -79,7 +57,7 @@ private:
     MonthData weatherData;
 
 
-    Statistics stats; // Add Statistics object
+
 
 
 };
