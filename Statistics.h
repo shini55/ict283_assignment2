6adfe3ce257748813bdae43748eda83585c9a84c
shini55 <tharshini2312@hotmail.com>
2025-04-01 19:56:45 +0800

    Update Statistics.h
    update stats class with BST objects

--- Statistics.h
+++ Statistics.h
@@ -1,25 +1,28 @@
 #ifndef STATISTICS_H_INCLUDED
 #define STATISTICS_H_INCLUDED
 
-#include "Vector.h"
-#include "Map.h"
+#include "WeatherLogType.h"
 #include "BinarySearchTree.h"
+#include "Map.h"
 #include <cmath>
-#include "WeatherRecType.h" // Include WeatherRecType.h
 
 class Statistics {
 public:
-    float calculateAverage(int avgType, int year, int month,
-                            const Map<int, Map<int, Vector<WeatherRecType>>>& weatherData,
-                            const Map<int, Map<int, BinarySearchTree<int>>>& weatherValues) const;
+    Statistics(const Map<int, Map<int, Vector<WeatherRecType>>>& weatherData);
 
-    float calculateStandardDeviation(int stdDeviationType, int year, int month,
-                                     const Map<int, Map<int, Vector<WeatherRecType>>>& weatherData,
-                                     const Map<int, Map<int, BinarySearchTree<int>>>& weatherValues) const;
+    float calculateAverage(int avgType, int year, int month) const;
+    float calculateStandardDeviation(int stdDeviationType, int year, int month) const;
+    float calculateTotalSolarRadiation(int year, int month) const;
+    float calculateSPCC(int combinationType, int year, int month) const;
+    float calculateMAD(int madType, int year, int month) const;
 
-    float calculateTotalSolarRadiation(int year, int month,
-                                       const Map<int, Map<int, Vector<WeatherRecType>>>& weatherData,
-                                       const Map<int, Map<int, BinarySearchTree<int>>>& weatherValues) const;
+private:
+    const Map<int, Map<int, Vector<WeatherRecType>>>& weatherData;
+    BinarySearchTree<int> validYears;
+    BinarySearchTree<int> validMonths;
+
+    void populateValidData();
+    bool isValidData(int year, int month) const;
 
 
 };
